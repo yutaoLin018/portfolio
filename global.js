4117d69cddfa63879a714a7b91e2c7fe11e7a208
Yutao Lin <lucius1098370452@gmail.com>
2026-04-16 01:16:49 -0700

    Update global.js

--- global.js
+++ global.js
@@ -20,9 +20,8 @@ document.body.insertAdjacentHTML(
 
 let select = document.querySelector('.color-scheme select');
 
-select.addEventListener('input', function (event) {
-  console.log('color scheme changed to', event.target.value);
-  document.documentElement.style.setProperty('color-scheme', event.target.value);
+select.addEventListener('change', function (event) {
+  document.documentElement.style.colorScheme = event.target.value;
 });
 
 const BASE_PATH =
