5de01440230479907521d1e8a7d03c6ca27856cc
Yutao Lin <lucius1098370452@gmail.com>
2026-04-16 00:02:35 -0700

    step 3.2

--- global.js
+++ global.js
@@ -27,5 +27,18 @@ for (let p of pages) {
     url = BASE_PATH + url;
   }
 
-  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${p.title}</a>`);
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
