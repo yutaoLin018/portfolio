44947d3512b58591b6a3fd11013c5f019d861527
Yutao Lin <lucius1098370452@gmail.com>
2026-04-15 23:47:52 -0700

    step 2

--- global.js
+++ global.js
@@ -1,5 +1,13 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add("current");
