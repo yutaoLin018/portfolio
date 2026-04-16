7ac8d052eb61575de4528de900b83e4617b3cd38
Yutao Lin <lucius1098370452@gmail.com>
2026-04-15 23:53:01 -0700

    step 3

--- global.js
+++ global.js
@@ -4,10 +4,28 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "cv/", title: "CV" },
+  { url: "https://github.com/yutaoLin018", title: "GitHub" }
+];
 
-currentLink?.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${p.title}</a>`);
+}
