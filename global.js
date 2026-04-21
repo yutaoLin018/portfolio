6a10653ca7fb14a7dd3da1bf46fe356e4d64db83
Yutao Lin <lucius1098370452@gmail.com>
2026-04-20 19:43:23 -0700

    step 3
    github api

--- global.js
+++ global.js
@@ -121,3 +121,18 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (!response.ok) {
+      throw new Error(`GitHub fetch failed: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching GitHub data:', error);
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -6,4 +6,25 @@ const projectsContainer = document.querySelector('.projects');
 
 if (projectsContainer && latestProjects) {
   renderProjects(latestProjects, projectsContainer, 'h2');
+}
+
+const githubData = await fetchGitHubData('yutaoLin018');
+const profileStats = document.querySelector('#profile-stats');
+
+if (githubData && profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos</dt>
+      <dd>${githubData.public_repos}</dd>
+
+      <dt>Followers</dt>
+      <dd>${githubData.followers}</dd>
+
+      <dt>Following</dt>
+      <dd>${githubData.following}</dd>
+
+      <dt>Public Gists</dt>
+      <dd>${githubData.public_gists}</dd>
+    </dl>
+  `;
 }
