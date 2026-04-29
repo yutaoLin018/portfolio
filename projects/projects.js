4e8c073ec7ed828ecae0bdf650d868fc855a5ece
Yutao Lin <lucius1098370452@gmail.com>
2026-04-29 14:39:45 -0700

    Lab 5 step 1
    step 1-1.5

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,26 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map(d => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
