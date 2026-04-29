b5bebaf760ff2bed4caa1f24432cc97f76c32d36
Yutao Lin <lucius1098370452@gmail.com>
2026-04-29 15:57:27 -0700

    Step 3

--- projects/projects.js
+++ projects/projects.js
@@ -6,14 +6,15 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
