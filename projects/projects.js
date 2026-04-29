a2fac410a35a3757f418097c9f0758bfb7f40843
Yutao Lin <lucius1098370452@gmail.com>
2026-04-29 14:43:32 -0700

    Step 2

--- projects/projects.js
+++ projects/projects.js
@@ -6,15 +6,24 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie()
+  .value((d) => d.value);
+
 let arcData = sliceGenerator(data);
-let arcs = arcData.map(d => arcGenerator(d));
+let arcs = arcData.map((d) => arcGenerator(d));
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
@@ -23,4 +32,14 @@ arcs.forEach((arc, idx) => {
     .append('path')
     .attr('d', arc)
     .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color: ${colors(idx)}`)
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
